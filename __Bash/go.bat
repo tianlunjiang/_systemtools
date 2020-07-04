84af4563062ab7bca1c86e84f078529b703aaf7c
TianlunPC <johnsonjtl@hotmail.com>
2020-07-04 13:58:32 -0700

    adapt to Cmder

--- __Bash/go.bat
+++ __Bash/go.bat
@@ -5,6 +5,7 @@ set KP_SCENE=%KP_SHOT:~0,3%
 set KP_SHOWDIR=k:/PROJECTS/Personal/%KP_SHOW%/
 set KP_SHOTDIR=k:/PROJECTS/Personal/%KP_SHOW%/%KP_SHOT%/
 set KP_SHELL=CMD
+set OCIO=K:\PROJECTS\_VFXtemplet\ocio-config\aces_1.0.3\config.ocio
 
 set argC=0
 for %%x in (%*) do Set /A argC+=1
@@ -14,6 +15,7 @@ if %argC% == 2 (
     if exist %KP_SHOWDIR% (
         if exist %KP_SHOTDIR% (
 
+            cls
             k:
             cd %KP_SHOTDIR%
             echo == GO TO SHOT =========
@@ -22,19 +24,28 @@ if %argC% == 2 (
             echo Shot:   %KP_SHOT%
             echo Dir:    %KP_SHOTDIR%
             echo =======================
-            dir /ad /b
+            REM dir /ad /b
+            ls
             echo =======================
-            echo "- cdnuke: nuke workbench dir"
-            echo "- cdmaya: maya workbench dir"
-            echo "- cdlgt:  lighting renders dir"
-            echo "- cddelivery:  delivery renders dir"
+            echo - cdshot: shot dir
+            echo - cdshow: show dir
+            echo - cdnuke: nuke workbench dir
+            echo - cdmaya: maya workbench dir
+            echo - cdblender: blender workbench dir
+            echo - cdlgt:  lighting renders dir
+            echo - cddailies:  dailies dir
+            echo - cddelivery:  delivery renders dir
             title %KP_SHOW%:%KP_SCENE%:%KP_SHOT%
 
+            doskey cdshot=cd "%KP_SHOTDIR%"
+            doskey cdshow=cd "%KP_SHOWDIR%"
             doskey cdnuke=cd "%KP_SHOTDIR%workbench/nuke/"
             doskey cdmaya=cd "%KP_SHOTDIR%workbench/maya/scenes/"
+            doskey cdblender=cd "%KP_SHOTDIR%workbench/blender/"
             doskey cdlgt=cd "%KP_SHOTDIR%assets/lighting/"
+            doskey cddailies=cd "%KP_SHOWDIR%_dailies/"
             doskey cddelivery=cd "%KP_SHOWDIR%_delivery/"
-            doskey ls=dir /a /b /od
+            REM doskey ls=dir /a /b /od
 
         ) else (
             echo.
