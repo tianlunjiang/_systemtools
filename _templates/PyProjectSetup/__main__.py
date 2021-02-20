9bebf26bb0baa1090e27de97a58b078750d05bdf
TianlunPC <johnsonjtl@hotmail.com>
2021-02-20 14:41:58 -0800

    restructure template scripts

---
+++ _templates/PyProjectSetup/__main__.py
@@ -0,0 +1,213 @@
+'''
+
+Python script to setup python project
+built to run in command-line
+
+'''
+
+
+
+
+#------------------------------------------------------------------------------
+#-Module Import
+#------------------------------------------------------------------------------
+
+
+
+
+import platform
+import os
+import sys
+import shutil
+import logging
+from kplogger import log, col, add_FileHandler
+
+
+
+
+#------------------------------------------------------------------------------
+#-Header
+#------------------------------------------------------------------------------
+
+
+
+
+__VERSION__='1.0'
+__OS__=platform.system()
+__AUTHOR__="Tianlun Jiang"
+__COPYRIGHT__="copyright %s" % __AUTHOR__
+
+__TITLE__=__name__
+
+
+def _version_():
+	ver='''
+
+	version 1.0
+	- Copy and Rename template
+
+	'''
+	return ver
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Set up logger
+#-------------------------------------------------------------------------------
+
+
+
+
+log_file = logging.getLogger('file_logger')
+log_file.setLevel(logging.INFO)
+add_FileHandler(log_file, 
+				os.path.join(os.path.dirname(__file__), 'ProjectSetup.log'))
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Global Variables
+#-------------------------------------------------------------------------------
+
+
+
+DIR_TEMPLATE = "D:/Dropbox/REPOSITORIES/_systemtools/_templates/template_PyProject_pkg"
+DIR_REPLACE = 'PyProject'
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Main Functions
+#-------------------------------------------------------------------------------
+
+
+
+
+def main(dir_cwd, name_project):
+	'''main function
+	@dir_cwd: (str) current directory
+	@name_project: (str) name of the project
+	'''
+
+	dir_new_proj = joinPath(dir_cwd, name_project+'_pkg')
+	log.info(">>> New project Path: \n    %s" % dir_new_proj)
+
+	try:
+
+		log.info(">>> Creating new directory from template")
+
+		shutil.copytree(DIR_TEMPLATE, dir_new_proj)
+
+		log.info(col.msg.DONE)
+
+	except:
+
+		log.error(col.ERROR + "Directory already exist, exit" + col.ENDLN)
+
+		exit()
+
+	project_tmp = joinPath(dir_new_proj, DIR_REPLACE)
+	project_mod = joinPath(dir_new_proj, name_project)
+	file_readme = joinPath(dir_new_proj, 'README.md')
+
+	log.debug("Temp Package: %s" % project_tmp)
+	log.debug("New Package: %s" % project_mod)
+	log.debug("README file: %s" % file_readme)
+
+	log.info(">>> Start renaming main module")
+
+	if os.path.isdir(project_tmp):
+		os.rename(project_tmp, project_mod)
+
+		log.info(col.msg.DONE)
+		
+	else:
+		log.error(col.ERROR + "Main Module name replace error, exit" + col.ENDLN)
+		exit()
+
+	editREADME(file_readme, name_project)
+	printDirTree(dir_new_proj)
+	
+	log.info('\n\n' + '='*20 + "\n%s is setup!" % name_project)
+
+	return dir_new_proj
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Support Function
+#-------------------------------------------------------------------------------
+
+
+
+
+def joinPath(*args):
+	try:
+		return os.path.join(*args).replace('\\', '/')
+	except: pass
+
+
+def printDirTree(dir):
+	print ('\n'+'='*20 +'\nListing Project Directries\n')
+	
+	for root, dirs, files in os.walk(dir):
+		level = root.replace(dir, '').count(os.sep)
+		indent = ' ' * 4 * (level)
+		print('{}{}/'.format(indent, os.path.basename(root)))
+		subindent = ' ' * 4 * (level + 1)
+		for f in files:
+			print('{}{}{}{}'.format(subindent,col.DISABLE, f, col.ENDLN))
+
+
+def editREADME(file, name_project):
+	'''source: https://www.kite.com/python/answers/how-to-edit-a-specific-line-in-a-text-file-in-python#:~:text=Use%20file.,at%20a%20certain%20line%20number.'''
+	
+	print('-'*20)
+	des = input('\n' + col.bg.CYAN + " Add oneline description of {}: \n".format(name_project) + col.ENDLN)
+	print('-'*20)
+
+	if des and name_project:
+
+		log.info("Edit project name and oneline description")
+
+		with open(file, 'r') as r:
+			lines = r.read()
+			lines = lines.format(name_project=name_project, description=des)
+			with open(file, 'w') as w:
+				w.write(lines)
+
+		log.info(col.msg.DONE)
+
+	else:
+		log.error("Error editing README.md")
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Execution
+#-------------------------------------------------------------------------------
+
+
+
+
+if __name__ == '__main__':
+
+	dir_cwd = os.getcwd().replace('\\', '/')
+	log.debug("current directory: %s" % dir_cwd)
+
+	if len(sys.argv) >= 2:
+		if len(sys.argv) > 2:
+			log.error("Command-line only takes 1 input, take 1st one instead")
+		name_project = sys.argv[1]
+		main(dir_cwd, name_project)
+		
+		log_file.info("%s created! (%s)" % (name_project, joinPath(dir_cwd,name_project)))
+		
+	elif len(sys.argv) < 2:
+		log.critical("Command-line needs an name input")
+		
---
+++ _templates/PyProjectSetup/kplogger.py
@@ -0,0 +1,241 @@
+'''
+
+custom logging object and module
+
+'''
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Import Modules
+#-------------------------------------------------------------------------------
+
+
+
+
+import logging
+import sys
+import os
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Global Variable
+#-------------------------------------------------------------------------------
+
+
+
+
+LEVEL_TO_STR = {
+	10: 'DEBUG',
+	20: 'INFO',
+	30: 'WARNING',
+	40: 'ERROR',
+	50: 'CRITICAL'
+}
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Terminal Foramts
+#-------------------------------------------------------------------------------
+
+
+
+
+class col:
+
+	# Per TYPE
+	LEVEL 			= ' \033[1;37;45m '
+	LINENUM			= ' \033[0;30;47m '
+	MODULE			= ' \033[1;33;42m '
+	MSG 			= ' \033[0m '
+
+	# Themes Per Status
+	DEBUG			= ' \033[1;37;44m '
+	INFO			= ' \033[2;30;46m '
+	WARNING			= ' \033[2;30;43m '
+	ERROR			= ' \033[1;31;40m '
+	CRITICAL		= ' \033[1;33;41m '
+
+	# Basic Components
+	RESET			= '\033[0m'
+	ENDLN			= '\033[0m'
+	BOLD			= '\033[1m'
+	DISABLE			= '\033[2m'
+	ITALIC			= '\033[3m'
+	UNDERLINE		= '\033[4m'
+	REVERSE			= '\033[7m'
+	INVISIBLE		= '\033[8m'
+	STRIKE			= '\033[9m'
+
+	class fg:
+		RED			= '\033[1;31m'
+		GREEN		= '\033[1;32m'
+		BLUE		= '\033[1;34m'
+		CYAN		= '\033[1;36m'
+		YELLOW		= '\033[1;93m'
+		MAGENTA		= '\033[1;95m'
+
+	class bg:
+		RED			= '\033[1;41m'
+		GREEN		= '\033[1;42m'
+		BLUE		= '\033[1;44m'
+		CYAN		= '\033[1;46m'
+		YELLOW		= '\033[1;46m'
+		MAGENTA		= '\033[1;45m'
+
+	class msg:
+		'''message presets'''
+		DONE		= '\t\t\033[1;32m >>> DONE <<< \033[0m'
+
+
+
+	def bracket(col,msg):
+		'''bracketing string with coloring
+		@col: (obj) color object
+		@msg: (str) message with bracket with
+		return: (str) message bracked
+		'''
+		return str(col + msg + ' \033[0m')
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Logger
+#-------------------------------------------------------------------------------
+
+
+
+
+log = logging.getLogger(__name__)
+
+loglevel = os.getenv('KPENV') 
+if loglevel: print('==========\nlogging level: %s\n==========' % LEVEL_TO_STR[int(loglevel)]) 
+else: 
+	loglevel = logging.INFO
+	print('ENV not set, level set to 20:INFO')
+log.setLevel(int(loglevel))
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Custom Formatter Class
+#-------------------------------------------------------------------------------
+
+
+
+
+class KuFormatter(logging.Formatter):
+	'''source: https://stackoverflow.com/questions/1343227/can-pythons-logging-format-be-modified-depending-on-the-message-log-level/8349076'''
+
+
+	FORMATS = {
+		logging.DEBUG		: ''.join([col.DEBUG, 	"%(levelname)s", col.LINENUM, "Ln:%(lineno)d", col.MSG, "%(message)s", col.ENDLN]),
+		logging.INFO 		: ''.join([col.MSG, 		"%(message)s", col.ENDLN]),
+		logging.ERROR		: ''.join([col.ERROR, 	"%(levelname)s", col.LINENUM, "Ln:%(lineno)d", col.MSG, "%(message)s", col.ENDLN]),
+		logging.WARNING		: ''.join([col.WARNING, 	"%(levelname)s", col.LINENUM, "Ln:%(lineno)d", col.MSG, "%(message)s", col.ENDLN]),
+		logging.CRITICAL	: ''.join([col.CRITICAL, "%(levelname)s", col.LINENUM, "Ln:%(lineno)d", col.MSG, "%(message)s", col.ENDLN]),
+	}
+
+	def format(self, record):
+		log_fmt = self.FORMATS[record.levelno]
+		formatter = logging.Formatter(log_fmt)
+		return formatter.format(record)
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Handlers and Formatters
+#-------------------------------------------------------------------------------
+
+
+
+# Create Handler
+handler_stream = logging.StreamHandler()
+
+# Set Formatter
+formatter_stream = KuFormatter()
+
+# Assign Formatter and set handler levels
+handler_stream.setFormatter(formatter_stream)
+
+# Add Handlers
+log.addHandler(handler_stream)
+
+
+def add_FileHandler(log, logname, fmt="%(asctime)s, %(message)s"):
+	'''create, set and add file handler
+	@log: (obj) logging object
+	@logname: (str) name of the log file
+	@fmt: (str) format for file handler
+	return: (obj) log object with FileHandler added
+	'''
+
+	_handler = logging.FileHandler(logname)
+	_formatter = logging.Formatter(fmt,"%Y-%m-%d %H:%M:%S")
+	_handler.setFormatter(_formatter)
+	# _handler.setLevel(logging.INFO) # Only to log Error level
+	
+	# Add Handler
+	log.addHandler(_handler)
+
+	return log
+
+
+
+
+#-------------------------------------------------------------------------------
+#-Testing Display
+#-------------------------------------------------------------------------------
+
+
+
+
+if __name__ == '__main__':
+	'''for displaying testing'''
+
+	log.setLevel(logging.DEBUG)
+
+	# str_d = (col.DEBUG, '10 debug message', col.MSG , "Detailed information, Diagnosing problems.")
+	# str_i = (col.INFO, '20 Info message', col.MSG , "Confirmation that things are working as expected.")
+	# str_w = (col.WARNING, '30 warning message', col.MSG , "An indication that something unexpected happened")
+	# str_e = (col.ERROR, '40 error message', col.MSG , "The software has not been able to perform some function.")
+	# str_c = (col.CRITICAL, '50 critical message', col.MSG , "The program itself may be unable to continue running.")
+
+	# log.debug(''.join(str_d))
+	# log.info(''.join(str_i))
+	# log.warning(''.join(str_w))
+	# log.error(''.join(str_e))
+	# log.critical(''.join(str_c))
+
+	log.debug("Debug Message")
+	log.info("Info Message")
+	log.warning("Warning Message")
+	log.error("Error Message")
+	log.critical("critical Message")
+
+	# print('\n' + col.MSG + "FG Test Color" + col.ENDLN)
+	# print(col.fg.RED + 'fg red'+ col.ENDLN)
+	# print(col.fg.GREEN + 'fg green'+ col.ENDLN)
+	# print(col.fg.BLUE + 'fg blue'+ col.ENDLN)
+	# print(col.fg.CYAN + 'fg cyan'+ col.ENDLN)
+	# print(col.fg.YELLOW + 'fg yellow'+ col.ENDLN)
+	# print(col.fg.MAGENTA + 'fg magenta'+ col.ENDLN)
+
+	# print('\n' + col.MSG + "BG Test Color"+ col.ENDLN)
+	# print(col.bg.RED + 'bg red'+ col.ENDLN)
+	# print(col.bg.GREEN + 'bg green'+ col.ENDLN)
+	# print(col.bg.BLUE + 'bg blue'+ col.ENDLN)
+	# print(col.bg.CYAN + 'bg cyan'+ col.ENDLN)
+	# print(col.bg.YELLOW + 'bg yellow'+ col.ENDLN)
+	# print(col.bg.MAGENTA + 'bg magenta'+ col.ENDLN)
+
+	# print('\n' + col.MSG + "Bracked Color"+ col.ENDLN)
+	# print("this message is"+ col.bracket(col.fg.GREEN, "Bracked") + "with \'col.bracket()\'")
